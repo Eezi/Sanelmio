e5cd0c624c576d3465dbd6b4c8f72b71f5840797
Eezi <tomi.a.salo@outlook.com>
2023-07-16 18:48:45 +0300

    Added services and contact page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'hsl(262, 46%, 62%)',
+        secondary: 'hsl(33, 100%, 68%)',
+      }
+    },
   },
   plugins: [],
   darkMode: ['class', '.darkmode'],
